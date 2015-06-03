9a6f4dce0fd396fccf64a0f56a2a14742fa34289
Coen Hyde <coen.hyde@gmail.com>
2015-06-02 17:23:23 -0700

    adding Terraform to libs

--- source/65_aws.sh
+++ source/65_aws.sh
@@ -1,2 +1,3 @@
-export PATH=~/.dotfiles/libs/aws-autoscaling-cli/bin:$PATH
-export AWS_AUTO_SCALING_HOME=~/.dotfiles/libs/aws-autoscaling-cli
+# Add terraform to the PATH
+PATH=$(path_remove ~/.dotfiles/libs/aws-autoscaling-cli/bin):~/.dotfiles/libs/aws-autoscaling-cli/bin
+AWS_AUTO_SCALING_HOME=~/.dotfiles/libs/aws-autoscaling-cli
---
+++ source/66_terraform.sh
@@ -0,0 +1,2 @@
+# Add terraform to the PATH
+PATH=$(path_remove ~/.dotfiles/libs/terraform-0.5.3):~/.dotfiles/libs/terraform-0.5.3
