b2894a9370345f7a0e5f5c10c845aff0d96859e9
Coen Hyde <coen.hyde@gmail.com>
2015-07-22 16:35:31 -0700

    adding beer bundle exec shorthand

--- source/60_misc.sh
+++ source/60_misc.sh
@@ -1,9 +1,29 @@
+#!/bin/bash
+
+ROOT_PROJECT_DIR=$HOME/projects/
+
+proot() {
+  echo $(echo $PWD | sed "s|\($ROOT_PROJECT_DIR[A-Za-z]*\).*|\1|")
+}
+
 to() {
-  cd ~/projects/$1
+  cd $ROOT_PROJECT_DIR$1
 }
 
 _to_completion() {
-  COMPREPLY=($(compgen -W "$(ls ~/projects/)" ${COMP_WORDS[COMP_CWORD]}))
+  COMPREPLY=($(compgen -W "$(ls $ROOT_PROJECT_DIR)" ${COMP_WORDS[COMP_CWORD]}))
 }
 
 complete -F _to_completion to
+
+beer() {
+  spec=$1
+  shift
+  cd $(proot); bundle exec rspec ./spec/$spec $@
+}
+
+_beer_completion() {
+  COMPREPLY=($(compgen -W "$(cd $(proot); find ./spec -type f | sed 's|./spec/||')" ${COMP_WORDS[COMP_CWORD]}))
+}
+
+complete -F _beer_completion beer
