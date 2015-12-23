abda7576995977c2dd55d082e323f56d908a23fd
Coen Hyde <coen.hyde@gmail.com>
2015-12-22 16:48:23 -0800

    add go project navigation bash helper

--- source/73_golang.sh
+++ source/73_golang.sh
@@ -1,1 +1,12 @@
 export GOPATH=$HOME/golang
+export GO_PROJECTS_DIR="${GOPATH}/src/github.com/coen-hyde"
+
+gop() {
+  cd $GO_PROJECTS_DIR/$1
+}
+
+_gop_completion() {
+  COMPREPLY=($(compgen -W "$(ls $GO_PROJECTS_DIR)" ${COMP_WORDS[COMP_CWORD]}))
+}
+
+complete -F _gop_completion gop
