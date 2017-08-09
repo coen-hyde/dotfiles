bd112a299306ef031677fef798c4bcacbf399ddd
Coen Hyde <coen.hyde@gmail.com>
2017-08-09 10:11:51 -0700

    deleting aws source

--- source/65_aws.sh
+++
@@ -1,3 +0,0 @@
-# Add terraform to the PATH
-PATH=$(path_remove ~/.dotfiles/libs/aws-autoscaling-cli/bin):~/.dotfiles/libs/aws-autoscaling-cli/bin
-AWS_AUTO_SCALING_HOME=~/.dotfiles/libs/aws-autoscaling-cli
