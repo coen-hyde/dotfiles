f056dd200f7b7ecab7d99b70354ef2405da3e6e2
Coen Hyde <coen.hyde@gmail.com>
2017-08-13 12:15:46 -0700

    fix brew paths for curl and openssl

--- source/20_brew_paths.sh
+++ source/20_brew_paths.sh
@@ -1,1 +1,2 @@
-PATH=$(path_remove /usr/local/opt/openssl/bin):/usr/local/opt/openssl/bin
+PATH=/usr/local/opt/openssl/bin:$(path_remove /usr/local/opt/openssl/bin)
+PATH=/usr/local/opt/curl/bin:$(path_remove /usr/local/opt/curl/bin)
