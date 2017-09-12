1278f70c71c5db7496295864c96df2dffecb653a
Coen Hyde <coen.hyde@gmail.com>
2017-09-12 14:29:48 -0700

    adding postgres path to sources

--- source/20_brew_paths.sh
+++ source/20_brew_paths.sh
@@ -1,2 +1,3 @@
 PATH=/usr/local/opt/openssl/bin:$(path_remove /usr/local/opt/openssl/bin)
 PATH=/usr/local/opt/curl/bin:$(path_remove /usr/local/opt/curl/bin)
+PATH=/usr/local/opt/postgresql/bin/:$(path_remove /usr/local/opt/postgresql/bin/)
